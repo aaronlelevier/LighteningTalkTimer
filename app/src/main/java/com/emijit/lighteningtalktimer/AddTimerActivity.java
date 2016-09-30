f5a5afb72dbf2e194fe6c8780dd7ea8caba92acd
aronysidoro <pyaaron@gmail.com>
2016-09-30 05:37:13 -0700

    [layout] works to use the same parent 'activity_main' xml, and attach either the main-fragment, or the add-timer-fragment to it

---
+++ app/src/main/java/com/emijit/lighteningtalktimer/AddTimerActivity.java
@@ -0,0 +1,27 @@
+package com.emijit.lighteningtalktimer;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class AddTimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, new AddTimerActivityFragment())
+                .commit();
+    }
+
+}
--- app/src/main/java/com/emijit/lighteningtalktimer/MainActivity.java
+++ app/src/main/java/com/emijit/lighteningtalktimer/MainActivity.java
@@ -1,13 +1,13 @@
 package com.emijit.lighteningtalktimer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,14 +22,13 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, AddTimerActivity.class));
             }
         });
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new MainActivityFragment())
+                    .add(R.id.container, new TimersFragment())
                     .commit();
         }
     }
