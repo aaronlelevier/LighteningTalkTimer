88a89739535d7e7ccc375dd023c92aabc4c104b1
aronysidoro <pyaaron@gmail.com>
2016-10-01 06:28:35 -0700

    [Add timer toolbar] added w/ title, so User knows what view this is. Next add the 'set alter interval' view

--- app/src/main/java/com/emijit/lighteningtalktimer/addtimer/AddTimerActivity.java
+++ app/src/main/java/com/emijit/lighteningtalktimer/addtimer/AddTimerActivity.java
@@ -1,7 +1,6 @@
 package com.emijit.lighteningtalktimer.addtimer;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -15,11 +14,6 @@ public class AddTimerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_timer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        ActionBar ab = getSupportActionBar();
-        if (ab != null) {
-            ab.setDisplayHomeAsUpEnabled(true);
-        }
 
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.container, new AddTimerFragment())
