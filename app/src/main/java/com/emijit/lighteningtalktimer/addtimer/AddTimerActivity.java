847d848fe0d10b5e4f43d4dcd1e2219ad506c597
aronysidoro <pyaaron@gmail.com>
2016-10-01 09:44:07 -0700

    [Fragment architecture] instead of using a separate activities for Add Timer / Set Interval, will use 1 Activity and 2 Fragments

--- app/src/main/java/com/emijit/lighteningtalktimer/addtimer/AddTimerActivity.java
+++ app/src/main/java/com/emijit/lighteningtalktimer/addtimer/AddTimerActivity.java
@@ -6,18 +6,25 @@ import android.support.v7.widget.Toolbar;
 
 import com.emijit.lighteningtalktimer.R;
 
-public class AddTimerActivity extends AppCompatActivity {
+public class AddTimerActivity extends AppCompatActivity implements AddTimerFragment.Callback {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_timer);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.container, new AddTimerFragment())
                 .commit();
+    }
+
+    @Override
+    public void setToolbarTitle(String string) {
+        mToolbar.setTitle(string);
     }
 
 }
--- app/src/main/java/com/emijit/lighteningtalktimer/addtimer/AddTimerFragment.java
+++ app/src/main/java/com/emijit/lighteningtalktimer/addtimer/AddTimerFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.emijit.lighteningtalktimer.R;
 
@@ -13,12 +14,30 @@ import com.emijit.lighteningtalktimer.R;
  */
 public class AddTimerFragment extends Fragment {
 
+    private View rootView;
+
     public AddTimerFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_timer, container, false);
+        rootView = inflater.inflate(R.layout.fragment_add_timer, container, false);
+        addForwardBtn();
+        return rootView;
+    }
+
+    private void addForwardBtn() {
+        ImageButton btn = (ImageButton) rootView.findViewById(R.id.forward);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Callback) getActivity()).setToolbarTitle(getString(R.string.title_set_timer_interval));
+            }
+        });
+    }
+
+    interface Callback {
+        void setToolbarTitle(String string);
     }
 }
